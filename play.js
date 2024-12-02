3d24f589dd5364dd0299d55fcbb3b98bad8efca0
Nat Tuck <nat@ferrus.net>
2024-12-01 21:33:10 -0500

    complete

--- play.js
+++ play.js
@@ -1,30 +1,42 @@
 
-const name = "student";
-const game = "auto-" + name + "-" + roll(999);
 
-function roll(xx) {
-  return Math.floor(xx * Math.random());
-}
+const name = "student";
+const game = "auto-" + name + "-" + randomInt(1000);
 
 const alphabet = new Set("abcdefghijklmnopqrstuvwxyz".split(''));
 
-function onView(view) {
-  const guesses = new Set(view.guesses);
-  const moves = alphabet.difference(guesses);
-
-  console.log("alphabet", alphabet);
-  console.log("puzzle:", view.puzzle);
-  console.log("guesses:", guesses);
-  console.log("moves:", moves);
-}
-
-
+require('core-js/actual');
 let { Socket } = require('phoenix-channels');
-let socket = new Socket("wss://words.homework.quest/socket");
 
+let socket = new Socket("wss://words.homework.quest/socket", {debug: true});
 socket.connect();
 
 let channel = socket.channel("game:" + game, {name});
+
+function randomInt(xx) {
+  return Math.floor(xx * Math.random());
+}
+
+function randomPick(xs) {
+  return xs[randomInt(xs.length)];
+}
+
+function onView(view) {
+  const guesses = new Set(view.guesses);
+  const moves = Array.from(alphabet.difference(guesses));
+
+  console.log("alphabet", Array.from(alphabet));
+  console.log("puzzle:", view.puzzle);
+  console.log("guesses:", Array.from(guesses));
+  console.log("moves:", moves);
+
+  if (moves.length > 0) {
+    channel.push("guess", {ch: randomPick(moves)});
+  }
+  else {
+    console.log("done", view);
+  }
+}
 
 channel.join()
   .receive("ok", (msg) => console.log("Connected to game:", msg.game))
